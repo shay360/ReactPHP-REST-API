494644db2fe9de5fc700c13393db834c4d41e3b1
Shay Zeevi <shay@zogomarket.com>
2020-12-03 14:34:43 +0200

    basic server listening is working, now we need to make it a little more workable

--- server.php
+++ server.php
@@ -1,7 +1,22 @@
 <?php
 
+use Psr\Http\Message\ServerRequestInterface;
+use React\EventLoop\Factory;
+use React\Http\Message\Response;
+use React\Http\Server;
+use React\Socket\Server as SocketServer;
+
 require 'vendor/autoload.php';
 
-$loop = \React\EventLoop\Factory::create();
-
+$loop = Factory::create();
+$server = new Server($loop, function (ServerRequestInterface $request) {
+   return new Response(
+      200,
+      ['Content-Type' => 'application/json'],
+      json_encode(['message' => 'REST API response'])
+   );
+});
+$socketServer = new SocketServer('127.0.0.1:8000', $loop);
+$server->listen($socketServer);
+echo 'Listening on: ' . str_replace('tcp', 'http', $socketServer->getAddress()) . PHP_EOL;
 $loop->run();
