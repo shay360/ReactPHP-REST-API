d7f9da51cecd9b07a8ca2e31e162ad5c0fd96f05
Shay Zeevi <shay@zogomarket.com>
2020-12-03 14:52:08 +0200

    basic example for working with a fast route

--- server.php
+++ server.php
@@ -1,20 +1,40 @@
 <?php
 
+use FastRoute\RouteCollector;
 use Psr\Http\Message\ServerRequestInterface;
 use React\EventLoop\Factory;
 use React\Http\Message\Response;
 use React\Http\Server;
 use React\Socket\Server as SocketServer;
+use function FastRoute\simpleDispatcher;
 
 require 'vendor/autoload.php';
 
+$routerDispatcher = simpleDispatcher(function (RouteCollector $routes) {
+   $routes->get('/games', function (ServerRequestInterface $request) {
+      return new Response(
+         200, ['Content-Type' => 'application/json'],
+         json_encode(['message' => 'Get working'])
+      );
+   });
+});
+
+
 $loop = Factory::create();
-$server = new Server($loop, function (ServerRequestInterface $request) {
-   return new Response(
-      200,
-      ['Content-Type' => 'application/json'],
-      json_encode(['message' => 'REST API response'])
+$server = new Server($loop, function (ServerRequestInterface $request) use ($routerDispatcher) {
+   $routerInfo = $routerDispatcher->dispatch(
+      $request->getMethod(), $request->getUri()->getPath()
    );
+   switch ($routerInfo[0]) {
+      case \FastRoute\Dispatcher::NOT_FOUND:
+         return new Response(404, ['Content-Type' => 'application/json'], json_encode(['message' => 'Not Found']));
+      case \FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+         return new Response(405, ['Content-Type' => 'application/json'], json_encode(['message' => 'Method Not Allowed']));
+      case \FastRoute\Dispatcher::FOUND:
+         return $routerInfo[1]($request);
+   }
+
+   throw new LogicException('No Service');
 });
 $socketServer = new SocketServer('127.0.0.1:8000', $loop);
 $server->listen($socketServer);
