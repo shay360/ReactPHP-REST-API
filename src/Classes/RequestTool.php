751cbfcec7de6bd27d696cd8c434a204dcac5f16
Shay Zeevi <shay@zogomarket.com>
2020-12-06 13:54:46 +0200

    phpdoc

--- src/Classes/RequestTool.php
+++ src/Classes/RequestTool.php
@@ -6,6 +6,11 @@ use Psr\Http\Message\ServerRequestInterface;
 
 class RequestTool {
 
+   /**
+    * Get request body json and return it decoded to php array
+    * @param ServerRequestInterface $request
+    * @return mixed
+    */
    public static function getRequestBody(ServerRequestInterface $request) {
       return json_decode($request->getBody()->getContents());
    }
