d0086c318100717db75a110871fa63a9d572bd4e
Shay Zeevi <shay@zogomarket.com>
2020-12-06 13:43:00 +0200

    phpdoc

--- src/Controllers/UsersBalance/GetUserBalance.php
+++ src/Controllers/UsersBalance/GetUserBalance.php
@@ -7,6 +7,14 @@ use Psr\Http\Message\ServerRequestInterface;
 use React\Http\Message\Response;
 
 final class GetUserBalance {
+   /**
+    * For this example we can see how i combine a path param (userID) and
+    * a decoded body json param $request->getBody()->getContents
+    * We can use coth of them to manipulate data
+    * @param ServerRequestInterface $request
+    * @param int $userID
+    * @return Response
+    */
    public function __invoke(ServerRequestInterface $request, int $userID) {
       $requestBody = json_decode($request->getBody()->getContents()); // Get the body json of the request
       $requestData['statusCode'] = 200;
