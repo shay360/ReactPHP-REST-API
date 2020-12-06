d5275f3c8e9ada5b2b1ce204d1ff926e6ffc3246
Shay Zeevi <shay@zogomarket.com>
2020-12-06 14:15:54 +0200

    added basic env file

--- migrations-db.php
+++ migrations-db.php
@@ -1,1 +1,11 @@
 <?php
+
+use App\Classes\Config;
+
+return [
+   'dbname' => getenv('DB_NAME'),
+   'user' => getenv('DB_USER'),
+   'password' => getenv('DB_PASS'),
+   'host' => getenv('DB_HOST'),
+   'driver' => getenv('DB_DRIVER')
+];
--- src/Classes/Config.php
+++ src/Classes/Config.php
@@ -1,12 +1,12 @@
 <?php
 
 namespace App\Classes;
-// @TODO Change required data before deploying to production
+
 class Config {
 
    public static function getEnvSettings() {
       return [
-         'isProduction' => false // Change to ttru on production
+         'isProduction' => false
       ];
    }
 }
